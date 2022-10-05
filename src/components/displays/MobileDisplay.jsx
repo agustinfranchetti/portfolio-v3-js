1d67956a4c036cbaa6ab8da1b25d7f2e30dd7755
agustinfranchetti <agustinfranchetti@gmail.com>
2022-10-05 17:57:16 -0300

    mobile kinda done

--- src/components/displays/MobileDisplay.jsx
+++ src/components/displays/MobileDisplay.jsx
@@ -18,35 +18,53 @@ const rowIconsDict = {
     4: "instagram",
   },
   2: {
-    1: "linkedin",
-    2: "github",
-    3: "twitter",
-    4: "instagram",
+    1: "location",
+    2: "discord",
+    3: null,
+    4: null,
   },
   3: {
-    1: "linkedin",
-    2: "github",
-    3: "twitter",
-    4: "instagram",
-  },
-  4: {
     1: "phone",
     2: "telegram",
     3: "email",
-    4: "document",
+    4: "resume",
   },
 };
 
 const iconIconsDict = {
-  linkedin: "https://img.icons8.com/color/256/000000/linkedin.png",
-  github: "https://img.icons8.com/color/256/000000/github--v1.png",
-  twitter: "https://img.icons8.com/color/256/000000/twitter--v1.png",
-  instagram: "https://img.icons8.com/color/256/000000/instagram-new--v1.png",
-  maps: "https://img.icons8.com/color/256/000000/google-maps-new.png",
-  email: "https://img.icons8.com/color/256/000000/email.png",
-  phone: "https://img.icons8.com/color/256/000000/phone.png",
-  telegram: "https://img.icons8.com/color/256/000000/telegram-app--v1.png",
-  document: "https://img.icons8.com/color/256/000000/document.png",
+  linkedin:
+    "https://cdn2.iconfinder.com/data/icons/social-media-applications/64/social_media_applications_14-linkedin-1024.png",
+  github:
+    "https://cdn3.iconfinder.com/data/icons/social-rounded-2/72/GitHub-1024.png",
+  twitter:
+    "https://cdn2.iconfinder.com/data/icons/social-media-applications/64/social_media_applications_6-twitter-1024.png",
+  instagram:
+    "https://cdn2.iconfinder.com/data/icons/social-media-applications/64/social_media_applications_3-instagram-1024.png",
+  location: "https://img.icons8.com/color/344/apple-map.png",
+  discord:
+    "https://cdn2.iconfinder.com/data/icons/gaming-platforms-squircle/250/discord_squircle-1024.png",
+  phone:
+    "https://cdn2.iconfinder.com/data/icons/apple-ios7-icons/118/phone_ios7_ios_7.png",
+  email:
+    "https://cdn0.iconfinder.com/data/icons/apple-apps/100/Apple_Mail-1024.png",
+  telegram:
+    "https://cdn2.iconfinder.com/data/icons/social-media-applications/64/social_media_applications_19-telegram-1024.png",
+  resume:
+    "https://cdn2.iconfinder.com/data/icons/apple-ios7-icons/118/notes_ios7_ios_7.png",
+};
+
+const iconLinksDict = {
+  linkedin: "https://www.linkedin.com/in/agustinfranchetti/",
+  github: "https://github.com/agustinfranchetti",
+  twitter: "https://twitter.com/franchoDev",
+  instagram: "https://www.instagram.com/agustinfranchetti/",
+  location: "https://goo.gl/maps/6Z5Z9Z9Z9Z9Z9Z9Z9",
+  discord: "https://discord.gg/franchomister#1996",
+  phone: "tel:+5492611234567",
+  email: "mailto: agustinfranchetti@gmail.com",
+  telegram: "https://t.me/agusfranchetti",
+  resume:
+    "https://drive.google.com/file/d/1Zv1sZdU6ZQ2QG8W8OwJz2fK6z1OzJ1eZ/view?usp=sharing",
 };
 
 const IconRowButton = ({ label }) => {
@@ -55,11 +73,13 @@ const IconRowButton = ({ label }) => {
       <Button
         colorScheme="whiteAlpha"
         variant="ghost"
-        onClick={() => console.log("clicked")}
+        onClick={() => window.open(iconLinksDict[label], "_blank")}
         h="16"
         w="16"
         bgImage={`url(${iconIconsDict[label]})`}
         bgSize="cover"
+        bgPosition="center"
+        borderRadius={"15px"}
       />
       <Text color={"white"} fontSize={"sm"}>
         {label}
@@ -99,15 +119,15 @@ export const MobileDisplay = () => {
         templateRows="repeat(5, 1fr)"
         // backgroundColor="red"
         mx={5}
-        mt={5}
+        my={2.5}
         height="fit-content"
         gap={2}
       >
         <IconsRow rowNumber={1} />
         <IconsRow rowNumber={2} />
-        <IconsRow rowNumber={3} />
         <Grid />
-        <IconsRow rowNumber={4} />
+        <Grid />
+        <IconsRow rowNumber={3} />
       </Grid>
     </Box>
   );
