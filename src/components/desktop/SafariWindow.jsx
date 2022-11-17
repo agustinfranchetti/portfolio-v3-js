b47579b3ba0ae30bd1102f7b0ebad6c24e9e9609
agustinfranchetti <agustinfranchetti@gmail.com>
2022-11-17 18:32:09 -0300

    Update SafariWindow.jsx

--- src/components/desktop/SafariWindow.jsx
+++ src/components/desktop/SafariWindow.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useContext } from "react";
 import { DesktopContext } from "./DesktopDisplay";
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Heading,
+  Button,
+} from '@chakra-ui/react'
 
 export const SafariWindow = () => {
   const { zIndexes, handleSetAsTopScreen } = useContext(DesktopContext);
@@ -12,10 +20,11 @@ export const SafariWindow = () => {
   return (
     <Box
       width="600px"
-      height="400px"
-      backgroundColor="gray.700"
+      height="500px"
+      backgroundColor="white"
       borderRadius="10px"
       padding="10px"
+      mt={"30px"}
       mr={"200px"}
       boxShadow="dark-lg"
       position={"absolute"}
@@ -23,6 +32,24 @@ export const SafariWindow = () => {
       onClick={() => handleSetAsTopScreen("safari")}
       resize="both"
       overflow={"auto"}
-    />
+    >
+      <FormControl id="email">
+        <Heading as="h1" size="lg" mb={"10px"}>
+          Send me an email
+        </Heading>
+        <FormLabel>Full name</FormLabel>
+        <Input type="text" />
+        <FormLabel>Email address</FormLabel>
+        <Input type="email" />
+        <FormErrorMessage>Error message</FormErrorMessage>
+        <FormLabel>Subject</FormLabel>
+        <Input type="text" />
+        <FormLabel>Message</FormLabel>
+        <Input type="text" h={"100px"} />
+      </FormControl>
+      <Button mt={4} colorScheme="teal" type="submit">
+        Submit
+      </Button>
+    </Box>
   );
 };
