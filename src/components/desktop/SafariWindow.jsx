520e2fe5b05d17f1748bfc2561df2263ec248771
agustinfranchetti <agustinfranchetti@gmail.com>
2022-11-18 19:20:41 -0300

    added react-icons for window buttons

--- src/components/desktop/SafariWindow.jsx
+++ src/components/desktop/SafariWindow.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext } from "react";
 import { DesktopContext } from "./DesktopDisplay";
+import { FaCircle, FaDotCircle } from "react-icons/fa";
 import {
   Box,
   FormControl,
@@ -8,7 +9,8 @@ import {
   Input,
   Heading,
   Button,
-} from '@chakra-ui/react'
+  Flex,
+} from "@chakra-ui/react";
 
 export const SafariWindow = () => {
   const { zIndexes, handleSetAsTopScreen } = useContext(DesktopContext);
@@ -20,10 +22,9 @@ export const SafariWindow = () => {
   return (
     <Box
       width="600px"
-      height="500px"
-      backgroundColor="white"
+      height="520px"
+      backgroundColor="#f4f5f7"
       borderRadius="10px"
-      padding="10px"
       mt={"30px"}
       mr={"200px"}
       boxShadow="dark-lg"
@@ -32,8 +33,21 @@ export const SafariWindow = () => {
       onClick={() => handleSetAsTopScreen("safari")}
       resize="both"
       overflow={"auto"}
-      p={5}
+      px={5}
+      pt={5}
     >
+      <Flex
+        justifyContent="flex-start"
+        alignItems="center"
+        direction={"row"}
+        gap={2}
+        mt={-2}
+        mb={5}
+      >
+        <FaCircle color="#d9515d" size={16} />
+        <FaCircle color="#f4c025" size={16} />
+        <FaCircle color="#3fc930" size={16} />
+      </Flex>
       <FormControl id="email">
         <Heading as="h1" size="lg" mb={"10px"}>
           Send me an email
@@ -48,7 +62,13 @@ export const SafariWindow = () => {
         <FormLabel>Message</FormLabel>
         <Input type="text" h={"100px"} />
       </FormControl>
-      <Button mt={4} colorScheme="teal" type="submit">
+      <Button
+        mt={4}
+        bgColor="#007AFF"
+        color="whiteAlpha.900"
+        type="submit"
+        _hover={{ bgColor: "#00A3ff" }}
+      >
         Submit
       </Button>
     </Box>
